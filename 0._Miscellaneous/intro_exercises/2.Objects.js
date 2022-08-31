401a3954b414ff3c5d3e15fccc7b2beddeb4182e
Jens-k-m-m <jensmonteiro@gmail.com>
2022-08-31 20:37:53 +0200

    2.Basics.js er lavet

--- 0._Miscellaneous/intro_exercises/2.Objects.js
+++ 0._Miscellaneous/intro_exercises/2.Objects.js
@@ -7,31 +7,49 @@ const alienMessage = {
 	message: "Hello, earthling! I bring peace."
 };
 
+console.log("Exercise 1: " + alienMessage.message);
+
 // Log the message 
 
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
+const id = {
+	name:"Jens",
+	 age:29}
 
+console.log("Exercise 2: Name: " + id.name + " age: " + id.age);
 
 // --------------------------------------
 // Exercise 3 - Add a property 
 
-const stackOverflow = {};
+const stackOverflow = {
+
+	isAllowed: true
+
+};
 
 // make a rule called isAllowed and let the value be true
+
+console.log("Exercise 3: "+stackOverflow.isAllowed);
 
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
 const thisSong = {
 	description: "The best song in the world."
+	
+	
 }
+
+delete thisSong.description;
+thisSong.about = "Just a tribute.";
+
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
-
+console.log("Exercise 4: " + thisSong.description +": thisSong.description. "+ "about: " + thisSong.about);
 // --------------------------------------
 
 
